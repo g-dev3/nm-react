80af656eb1fc3d8ef72adade66a9916681abb9c9
Gopal <gdev3@Gopals-MacBook-Air.local>
2025-12-27 07:11:33 +0530

    Migrated project from Parcel to Vite

--- App.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const div = document.createElement("div");
-  const text = document.createTextNode("Hello World");
-  div.className = "root";
-  div.appendChild(text);
-  document.body.appendChild(div);
-});
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const Title = ()=>{
+  return <h1>Namaste react using jsx</h1>
+}
+
+export default Title
